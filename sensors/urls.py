986fb11d48f8aebd5181a3647075e7aee3e4cb78
marahhanini <marahhanini2@gmail.com>
2024-07-17 21:53:53 +0300

    TR/BE:task1

--- sensors/urls.py
+++ sensors/urls.py
@@ -3,6 +3,7 @@ from . import views
 from .views import PressureSensorList, PressureReadingList
 
 urlpatterns = [
-    path('pressure_sensors/', PressureSensorList.as_view(), name='pressure_sensor_list'),
+  
+  path('pressure_sensors/', PressureSensorList.as_view(), name='pressure_sensor_list'),
     path('pressure_readings/', PressureReadingList.as_view(), name='pressure_reading_list'),
 ]
