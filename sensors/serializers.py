07b3b146cb491257b5040b6200fa6195449271af
marahhanini <marahhanini2@gmail.com>
2024-07-18 22:15:52 +0300

    TR/BE:task1

--- sensors/serializers.py
+++ sensors/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import PressureSensor, PressureReading
+from django.utils.dateparse import parse_datetime
 
 class PressureSensorSerializer(serializers.ModelSerializer):
     class Meta:
@@ -7,6 +8,20 @@ class PressureSensorSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class PressureReadingSerializer(serializers.ModelSerializer):
+    datetime = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.%fZ", input_formats=["%Y-%m-%dT%H:%M:%S.%fZ"])
+
     class Meta:
         model = PressureReading
         fields = '__all__'
+
+    def to_representation(self, instance):
+       # Convert datetime to ISO format 
+        representation = super().to_representation(instance)
+        representation['datetime'] = instance.datetime.isoformat()
+        return representation
+
+    def to_internal_value(self, data):
+        # Parse datetime from ISO format 
+        if 'datetime' in data:
+            data['datetime'] = parse_datetime(data['datetime'])
+        return super().to_internal_value(data)
